d55d81ebe27e80274487b9e8c4b404d6d3cc1483
Rishi Penmetcha <rp487@cornell.edu>
2022-05-27 22:36:32 -0700

    Added an about me

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 document.getElementById('defSideNav').focus();
+
+const aboutMe = "Hi, my name is Rishi. I am a Software Engineer at Palantir. "
+  + "I graduated from Cornell University in 2021 with an MEng in Computer Science "
+  + "and a BS in Applied Economics and Management. "
+  + "I like building software products and investing. "
+  + "In my free time I like to kick it with friends and spend time with family, "
+  + "either playing sports, eating good food, or exploring the outdoors.";
+
+document.getElementById('aboutMe').innerHTML = aboutMe;
+
