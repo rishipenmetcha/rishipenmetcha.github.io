4afe3528100c2b45b9dc76214ca35b2697412eb5
Rishi Penmetcha <rp487@cornell.edu>
2022-05-29 16:10:18 -0700

    added inputs

---
+++ inputs.js
@@ -0,0 +1,56 @@
+const aboutMe = "Hi, my name is Rishi. I am a Software Engineer at Palantir. "
+  + "I graduated from Cornell University in 2021 with an MEng in Computer Science "
+  + "and a BS in Applied Economics and Management. "
+  + "I like building software products and investing. "
+  + "In my free time I like to kick it with friends and spend time with family, "
+  + "either playing sports, eating good food, or exploring the outdoors.";
+
+const experience1 = {
+  company: "Palantir",
+  role: "Software Engineer",
+  time: "March 2022 - Present",
+  description: "Backend dev working on Gotham."
+}
+const experience2 = {
+  company: "Amazon Web Services (AWS)",
+  role: "Software Development Engineer Intern",
+  time: "June 2021 - August 2021",
+  description: "EC2 Fleet Insights"
+}
+const experience3 = {
+  company: "State Farm",
+  role: "Software Development Intern",
+  time: "May 2020 - July 2020",
+  description: ""
+}
+const experience4 = {
+  company: "Foothill Ventures",
+  role: "Intern Analyst",
+  time: "June 2018 - August 2018",
+  description: "Foothill Ventures (formerly Tsingyuan Ventures) is a "
+    + "technology-focused venture fund investing in seed to A-round startups."
+}
+
+const experience = [experience1, experience2, experience3, experience4];
+
+const education1 = {
+  school: "Cornell University",
+  degree: "MEng in Computer Science",
+  graduation: "December 2021"
+}
+const education2 = {
+  school: "Cornell University",
+  degree: "BS in Applied Economics and Management",
+  graduation: "December 2020"
+}
+const education3 = {
+  school: "Homestead High School",
+  degree: "",
+  graduation: "June 2017"
+}
+
+const education = [education1, education2, education3];
+
+const projects = "In progress...";
+
+export { aboutMe, experience, education, projects };
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+import { aboutMe } from "./inputs.js";
+
 document.getElementById('defSideNav').focus();
 
-const aboutMe = "Hi, my name is Rishi. I am a Software Engineer at Palantir. "
-  + "I graduated from Cornell University in 2021 with an MEng in Computer Science "
-  + "and a BS in Applied Economics and Management. "
-  + "I like building software products and investing. "
-  + "In my free time I like to kick it with friends and spend time with family, "
-  + "either playing sports, eating good food, or exploring the outdoors.";
+const sideSelected = (selection) => {
+  var obj = document.getElementById(selection);
+  obj.style['color'] = 'white';
+  obj.style['background-color'] = 'black';
+  obj.style['box-decoration-break'] = 'clone';
+}
 
-document.getElementById('aboutMe').innerHTML = aboutMe;
+document.getElementById('centerText').innerHTML = aboutMe;
 
