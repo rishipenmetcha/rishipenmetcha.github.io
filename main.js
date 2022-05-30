7594a12c84d749cc4223d34c3d5623b6e0ae34d9
Rishi Penmetcha <rp487@cornell.edu>
2022-05-29 17:55:24 -0700

    lol I printed the json straight up, should be good for a while

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-import { aboutMe } from "./inputs.js";
+import { aboutMe, experience, education, projects } from "./inputs.js";
 
-document.getElementById('defSideNav').focus();
+document.getElementById('aboutMe').addEventListener("focus",
+  function () {
+    display("centerText", aboutMe);
+  });
+document.getElementById('experience').addEventListener("focus",
+  function () {
+    display("centerText", JSON.stringify(experience, null, 2)
+      .replace(/\n( *)/g, function (match, p1) {
+        return '<br>' + '&nbsp;'.repeat(p1.length);
+      }));
+  });
+document.getElementById('education').addEventListener("focus",
+  function () {
+    display("centerText", JSON.stringify(education, null, 2)
+      .replace(/\n( *)/g, function (match, p1) {
+        return '<br>' + '&nbsp;'.repeat(p1.length);
+      }));
+  });
+document.getElementById('projects').addEventListener("focus",
+  function () {
+    display("centerText", projects);
+  });
 
-const sideSelected = (selection) => {
-  var obj = document.getElementById(selection);
-  obj.style['color'] = 'white';
-  obj.style['background-color'] = 'black';
-  obj.style['box-decoration-break'] = 'clone';
+function display(tab, text) {
+  document.getElementById(tab).innerHTML = text;
 }
 
-document.getElementById('centerText').innerHTML = aboutMe;
+document.getElementById('aboutMe').focus();
 
