0532bfa752943e25bd1d06fe5e589ae90718ee1e
Rishi Penmetcha <rp487@cornell.edu>
2022-05-29 18:08:01 -0700

    added click listeners

--- main.js
+++ main.js
@@ -23,6 +23,29 @@ document.getElementById('projects').addEventListener("focus",
     display("centerText", projects);
   });
 
+document.getElementById('aboutMe').addEventListener("click",
+  function () {
+    display("centerText", aboutMe);
+  });
+document.getElementById('experience').addEventListener("click",
+  function () {
+    display("centerText", JSON.stringify(experience, null, 2)
+      .replace(/\n( *)/g, function (match, p1) {
+        return '<br>' + '&nbsp;'.repeat(p1.length);
+      }));
+  });
+document.getElementById('education').addEventListener("click",
+  function () {
+    display("centerText", JSON.stringify(education, null, 2)
+      .replace(/\n( *)/g, function (match, p1) {
+        return '<br>' + '&nbsp;'.repeat(p1.length);
+      }));
+  });
+document.getElementById('projects').addEventListener("click",
+  function () {
+    display("centerText", projects);
+  });
+
 function display(tab, text) {
   document.getElementById(tab).innerHTML = text;
 }
