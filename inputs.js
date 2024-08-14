46d71448d4ef6366072cee2b534066641eecf629
rishipenmetcha <38026597+rishipenmetcha@users.noreply.github.com>
2024-08-14 16:01:56 -0400

    Update inputs.js

--- inputs.js
+++ inputs.js
@@ -1,37 +1,42 @@
-const aboutMe = "Hi, my name is Rishi. I am a Software Engineer at Palantir. "
-  + "I graduated from Cornell University in 2021 with an MEng in Computer Science "
-  + "and a BS in Applied Economics and Management. "
-  + "I like building software products and investing. "
-  + "In my free time I like to kick it with friends and spend time with family, "
-  + "either playing sports, eating good food, or exploring the outdoors.";
+const aboutMe = "Hi, my name is Rishi. I am currently a Software Engineer at Traba. "
 
-const experience1 = {
+const palantirExperience = {
   company: "Palantir",
   role: "Software Engineer",
-  time: "March 2022 - Present",
+  time: "March 2022 - March 2023",
   description: "Gotham Backend"
 }
-const experience2 = {
+const awsExperience = {
   company: "Amazon Web Services (AWS)",
   role: "Software Development Engineer Intern",
   time: "June 2021 - August 2021",
   description: "EC2 Fleet Insights"
 }
-const experience3 = {
+const stateFarmExperience = {
   company: "State Farm",
   role: "Software Development Intern",
   time: "May 2020 - July 2020",
-  description: ""
 }
-const experience4 = {
+const foothillVenturesExperience = {
   company: "Foothill Ventures",
   role: "Intern Analyst",
   time: "June 2018 - August 2018",
   description: "Foothill Ventures (formerly Tsingyuan Ventures) is a "
     + "technology-focused venture fund investing in seed to A-round startups."
 }
+const ironcladExperience = {
+  company: "Ironclad",
+  role: "Software Engineer",
+  time: "April 2023 - June 2024",
+  description: "AI Applications, Document"
+}
+const trabaExperience = {
+  company: "Traba",
+  role: "Software Engineer",
+  time: "July 2024 - Present",
+}
 
-const experience = [experience1, experience2, experience3, experience4];
+const experience = [trabaExperience, ironcladExperience, palantirExperience, awsExperience, stateFarmExperience, foothillVenturesExperience];
 
 const education1 = {
   school: "Cornell University",
@@ -45,12 +50,11 @@ const education2 = {
 }
 const education3 = {
   school: "Homestead High School",
-  degree: "",
   graduation: "June 2017"
 }
 
 const education = [education1, education2, education3];
 
-const projects = "In progress...";
+const writing = "Currently, half-baked drafts on my desktop. Will make their way here eventually."
 
-export { aboutMe, experience, education, projects };
+export { aboutMe, experience, education, writing };
